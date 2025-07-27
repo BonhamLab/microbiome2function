b84b85d298c5c312348ef62a1e8514ea81b40a38
Yehor Mishchyriak <yehormishchyriak@gmail.com>
2025-07-26 21:52:01 -0400

    wrote a bash script for submitting the parsing job

--- dataset_curation/for_hpc_use/data_mining.py
+++ dataset_curation/for_hpc_use/data_mining.py
@@ -2,17 +2,23 @@ from scripts import (recommended_fields_example2,
                      unirefs_from_multiple_files,
                      process_uniref_batches)
 import re
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
 gene_fam_files_dir = os.getenv("SAMPLE_FILES")
+output_dir = os.getenv("SAVE_DATA_TO_DIR")
+job_name = os.getenv("JOB_NAME")
+
+assert gene_fam_files_dir, "SAMPLE_FILES env var was not set!"
+assert output_dir, "SAVE_DATA_TO_DIR env var was not set!"
+assert job_name, "JOB_NAME env var was not set!"
 
 accession_nums = unirefs_from_multiple_files(gene_fam_files_dir, 
                                              pattern=re.compile(r".*_genefamilies\.tsv$"))
 
-output_dir = os.getenv("SAVE_DATA_TO_DIR")
+
+out = os.path.join(output_dir, job_name + "_output_dir")
+os.makedirs(out, exist_ok=True)
 
 process_uniref_batches(
     uniref_ids=accession_nums,
@@ -20,8 +26,8 @@ process_uniref_batches(
     batch_size=40_000,
     single_api_request_size=100,
     rps=10,
-    save_to_dir=output_dir,
+    save_to_dir=out,
     filter_out_bad_ids=True
 )
 
-print(f"Mined data is available at {output_dir}")
+print(f"Mined data is available at {out}")
---
+++ dataset_curation/for_hpc_use/job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -J uniprot_mine
+#SBATCH --time=3-10:00:00  #requested time (DD-HH:MM:SS)
+#SBATCH -p batch
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem=8g  #requesting 8GB of RAM total
+#SBATCH --output=uprotmine.%j.%N.out  #saving standard output to file, %j=JOBID, %N=NodeName
+#SBATCH --error=uprotmine.%j.%N.err   #saving standard error to file, %j=JOBID, %N=NodeName
+#SBATCH --mail-type=ALL    #email options
+#SBATCH --mail-user=ymishchyriak@wesleyan.edu
+
+source $HOME/miniconda3/etc/profile.d/conda.sh
+conda activate my_env
+
+cd /cluster/home/myehor01/data_processing/microbiome2function/dataset_curation/for_hpc_use
+
+export SAMPLE_FILES=/cluster/tufts/bonhamlab/shared/sequencing/processed/humann/main
+export SAVE_DATA_TO_DIR=/cluster/home/myehor01/data_processing
+export JOB_NAME=$SLURM_JOB_NAME
+
+python data_mining.py
